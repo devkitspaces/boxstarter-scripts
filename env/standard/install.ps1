aca6530fdbe4f4085b271222f64c57b0afd77c76
jrbeverly <jrbeverly@live.com>
2017-12-01 20:27:02 -0500

    Add standard install

---
+++ env/standard/install.ps1
@@ -0,0 +1,11 @@
+choco install -y git.install
+choco install -y foxitreader
+choco install -y googlechrome
+choco install keybase
+
+choco install -y malwarebytes
+choco install -y dropbox
+choco install -y visualstudiocode
+choco install -y virtualbox
+choco install -y docker docker-machine docker-compose
+choco install -y visualstudio2017community
